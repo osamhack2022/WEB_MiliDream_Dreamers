1aef32fc84f93f6f57db8d037e793976e79ee5c7
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-09-28 10:22:05 +0000

    [#3] Feat : Add prettier and .vscode folder
    - prettier를 dev 모드로 설치
    - prettierrc 파일 추가
    - 자동 설정을 위한 .vscode 폴더의 settings.json 추가

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,19 +1,18 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-      },
-      extends: [
-      'airbnb-base'
-      ],
-      parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-      },
-      rules: {
-      "no-console":"off",
-      "spaced-comment":"off",
-      "no-else-return":"off"
-      },
-}
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "off",
+    "spaced-comment": "off",
+    "no-else-return": "off",
+  },
+};
