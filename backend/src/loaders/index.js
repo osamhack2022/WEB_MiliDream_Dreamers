4709c75270f25d2810eaa40f173722e0a822efbf
inyeoptti <inyeoptti@github.com>
2022-09-30 14:30:11 +0000

    [#9] Fix: 로더 오타 수정 및 mariadb 로더 중 release부분 삭제

--- backend/src/loaders/index.js
+++ backend/src/loaders/index.js
@@ -1,10 +1,10 @@
-import expressLoader from 'express';
-import mariadbLoader from 'mariadb';
+import expressLoader from './express';
+import mariadbLoader from './mariadb';
 // import mysqlLoader from ''
 import Logger from './logger';
 
 export default async ({ expressApp }) => {
-	const mariaPool = await mariadbLoader;
+	const mariaPool = await mariadbLoader();
 	Logger.info('💿DB Loaded and Pool created📀');
 
 	await expressLoader({ app: expressApp });
--- backend/src/loaders/mariadb.js
+++ backend/src/loaders/mariadb.js
@@ -2,7 +2,7 @@ import mariadb from 'mariadb';
 import dbInfo from '../config/tmpdbconfig';
 
 
-export default function () {
+export default async function () {
 	const pool = mariadb.createPool({ //이 부분을 다른 곳으로 옮겨야 하나
 		host: dbInfo.DB_HOST,
 		port: dbInfo.DB_PORT,
@@ -12,19 +12,19 @@ export default function () {
 	});
 
 	return {
-		getConnection: function (callback) {
-			pool.getConnection(callback);
+		getConnection: async function (callback) {
+			return pool.getConnection(callback);
 		},
-		end: function (callback) {
-			pool.end(); // pool에서는 end 대신 release를 쓴다고 함 사실이라면 제거
-		},
-		release: function (callback) {
-			pool.release(); // pool을 사용하면 반드시 release ,
-			/*
-			정확하지 않음, 사용하지 않아도 된다고 하는 내용도.. 추가 조사 후 수정
-			relase()를 굳이 여기서 메소드로 안보내줘도 connection 함수 콜백 두번째 인수로 사용 가능한 것 같음
-			이 경우 relase는 지우기
-			*/
+		end: async function (callback) {
+			return pool.end();
 		}
+		/*
+		정확하지 않음, 사용하지 않아도 된다고 하는 내용도.. 추가 조사 후 수정
+		relase()를 굳이 여기서 메소드로 안보내줘도 connection 함수 콜백 두번째 인수로 사용 가능한 것 같음
+		이 경우 relase는 지우기
+		*/
+		//  release() 함수는 Pool이 아닌 PoolConnection에 대해 호출해줘야 합니다.
+		//  커넥션 가져다가 쓴 후 각 커넥션 사용 후 release() 함수 호출 -> 풀 사용 종료 시 end() 함수 호출
+		// }
 	};
 }
