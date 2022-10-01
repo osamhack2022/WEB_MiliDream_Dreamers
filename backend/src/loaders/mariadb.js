5e6b945366700c15f97f5e9fb712f914c9027910
SeonWoo KIm <sunwoo005@naver.com>
2022-10-01 07:08:26 +0000

    [#9] Comment: 필요 없어진 주석 제거

--- backend/src/loaders/mariadb.js
+++ backend/src/loaders/mariadb.js
@@ -18,13 +18,6 @@ export default async function () {
 		end: async function (callback) {
 			return pool.end();
 		}
-		/*
-		정확하지 않음, 사용하지 않아도 된다고 하는 내용도.. 추가 조사 후 수정
-		relase()를 굳이 여기서 메소드로 안보내줘도 connection 함수 콜백 두번째 인수로 사용 가능한 것 같음
-		이 경우 relase는 지우기
-		*/
-		//  release() 함수는 Pool이 아닌 PoolConnection에 대해 호출해줘야 합니다.
 		//  커넥션 가져다가 쓴 후 각 커넥션 사용 후 release() 함수 호출 -> 풀 사용 종료 시 end() 함수 호출
-		// }
 	};
 }
