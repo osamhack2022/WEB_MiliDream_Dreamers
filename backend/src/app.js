f12d0cc09145f77333d82b8f749a9c5a557e39c3
SeonWoo KIm <sunwoo005@naver.com>
2022-10-01 06:46:50 +0000

    [#9] Fix : app.js 로그 출력 변경

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,25 +1,26 @@
 import loaders from './loaders';
 import express, { application } from 'express';
+import { Logger } from 'winston';
 
 async function startServer() {
 
-    const PORT = process.env.PORT || 3000 ;
+	const PORT = process.env.PORT || 3000;
 
-    const app = express();
+	const app = express();
 
-    await loaders({ expressApp: app});
+	await loaders({ expressApp: app });
 
-    app.listen(PORT, (err)=>{
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(`
+	app.listen(PORT, (err) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		Logger.info(`
         ##########################################
          🛡️ Server listening on port: ${PORT} 🛡️
         ##########################################
         `);
-    });
+	});
 }
 
 startServer();
