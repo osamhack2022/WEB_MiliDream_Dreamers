d1643aa70c00a7d343f6f68faafa7a7b1ec9c5a5
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-10-04 10:41:25 +0000

    [#14] Fix : base.sql 업데이트
    - MBTI 테이블 삭제
    - 조회수, 추천인 추가

--- backend/base.sql
+++ backend/base.sql
@@ -1,41 +1,54 @@
-create database milidream_db;
-use milidream_db;
+create table Class(
+	classKey INT AUTO_INCREMENT PRIMARY KEY,
+	classContent TEXT /*CHARACTER SET utf8mb4*/ NOT NULL
+);
 
-create table Class(classKey INT AUTO_INCREMENT PRIMARY KEY,
-					classContent TEXT /*CHARACTER SET utf8mb4*/ NOT NULL);
+create table User (
+	userKey INT AUTO_INCREMENT PRIMARY KEY,
+	userName VARCHAR(20) UNIQUE KEY NOT NULL,
+	id VARCHAR(20) UNIQUE KEY NOT NULL,
+	passwd VARCHAR(64) NOT NULL,
+	classKey INT NOT NULL,
+	FOREIGN KEY(classKey) REFERENCES Class(classKey)
+);
 
-create table User (userKey INT AUTO_INCREMENT PRIMARY KEY, 
-				userName VARCHAR(20) UNIQUE KEY NOT NULL, 
-				id VARCHAR(20) UNIQUE KEY NOT NULL, 
-				passwd VARCHAR(64) NOT NULL,
-				classKey INT NOT NULL,
-				FOREIGN KEY(classKey) REFERENCES Class(classKey));
+create table Category(
+	categoryKey INT AUTO_INCREMENT PRIMARY KEY,
+	categoryName TEXT NOT NULL
+);
 
-create table Category(categoryKey INT AUTO_INCREMENT PRIMARY KEY, categoryName TEXT NOT NULL);
+create table Post (
+	postKey INT AUTO_INCREMENT PRIMARY KEY,
+	userkey INT NOT NULL,
+	postTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+	title TEXT NOT NULL,
+	body TEXT NOT NULL,
+	categoryKey INT NOT NULL,
+	careerPostKey INT,
+	viewCount INT,
+	FOREIGN KEY(categoryKey) REFERENCES Category(categoryKey),
+	FOREIGN KEY(careerPostKey) REFERENCES Post(postKey)
+);
 
-create table Post (postKey INT AUTO_INCREMENT PRIMARY KEY, 
-				userkey INT NOT NULL, 
-				postTime DATETIME DEFAULT CURRENT_TIMESTAMP, 
-				title TEXT NOT NULL, 
-				body TEXT NOT NULL, 
-				categoryKey INT NOT NULL, 
-				careerPostKey INT, 
-				FOREIGN KEY(categoryKey) REFERENCES Category(categoryKey), 
-				FOREIGN KEY(careerPostKey) REFERENCES Post(postKey));
+create table Recommenders(
+	recommenderKey INT AUTO_INCREMENT PRIMARY KEY,
+	postKey INT NOT NULL,
+	userKey INT NOT NULL,
+	FOREIGN KEY(postKey) REFERENCES Post(postKey),
+	FOREIGN KEY(userKey) REFERENCES User(userKey)
+);
 
-create table Comment(commentKey INT AUTO_INCREMENT PRIMARY KEY,
-					userKey INT NOT NULL,
-					content TEXT NOT NULL,
-					postKey INT NOT NULL,
-					commentTime DATETIME DEFAULT CURRENT_TIMESTAMP,
-					parentKey INT,
-					FOREIGN KEY(userKey) REFERENCES User(userKey),
-					FOREIGN KEY(postKey) REFERENCES Post(postKey),
-					FOREIGN KEY(parentKey) REFERENCES Comment(commentKey));
-
-create table MBTI(mbtiKEY INT AUTO_INCREMENT PRIMARY KEY,
-				mbtiType TEXT NOT NULL,
-				mbtiContent TEXT NOT NULL);
+create table Comment(
+	commentKey INT AUTO_INCREMENT PRIMARY KEY,
+	userKey INT NOT NULL,
+	content TEXT NOT NULL,
+	postKey INT NOT NULL,
+	commentTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+	parentKey INT,
+	FOREIGN KEY(userKey) REFERENCES User(userKey),
+	FOREIGN KEY(postKey) REFERENCES Post(postKey),
+	FOREIGN KEY(parentKey) REFERENCES Comment(commentKey)
+);
 
 insert into Class (classContent) values ("미정"), ("병사"), ("간부"), ("군무원");
 select * from Class;
