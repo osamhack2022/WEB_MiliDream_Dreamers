bf3a63b062686f35291cb6377432ac955e6bfaf0
Kim Ji-hwan <kjhwan0802@naver.com>
2022-10-06 14:55:32 +0000

    Design : SearchBar flex design
    - flex 속성을 통한 옆으로 길게 요소 배치
    
    Relate to :
    왼쪽 Navbar 위 게시판 검색 바(BoardSearchBar) 구현 #26

--- frontend/components/board/BoardSearchBar.js
+++ frontend/components/board/BoardSearchBar.js
@@ -4,12 +4,17 @@ export default function BoardSearchBar() {
   return (
     <div>
       {/* bootstrap */}
-      <nav className="navbar navbar-light bg-light">
+      <nav className="navbar navbar-light">
         <form className="form-inline">
           <input className="form-control mr-sm-2" type="search" placeholder="게시판 검색" aria-label="Search" />
           <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
       </nav>
+      <style jsx>{`
+        .form-inline {
+          display: flex;
+        }
+      `}</style>
     </div>
   )
 }
