34174768005ebcdf58a87d6906b514371ec43d78
SeonWoo KIm <sunwoo005@naver.com>
2022-10-07 13:51:59 +0000

    [#14] Remove: Rest API 변경으로 필요 없어진 파일 및 코드 제거

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -11,14 +11,7 @@ const Post = function (post) {
 	this.carrerPostKey = post.carrerPostKey;
 };
 
-// Post.create = async function (newPost) {
-// 	const conn = await mariadb.getConnection();
-// 	const sql = `INSERT INTO Post SET ?`;
-// 	const result = await conn.query(sql);
 
-// 	Logger.info(`Create Post: ${ {...newPost} }`)
-// 	return {...newPost};
-// }
 Post.getAll = async function (categoryKey) {
 	const sql = `SELECT * FROM Post WHERE categoryKey = ${categoryKey} ;`;
 	const result = await mariadb.query(sql);
