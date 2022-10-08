014a3301a388ee125284925aa9f9ac320fffd58b
inyeoptti <inyeoptti@github.com>
2022-10-08 08:53:54 +0000

    [#11] Feat: 세션 secret키 env파일에서 불러오도록 변경

--- backend/src/loaders/passport.js
+++ backend/src/loaders/passport.js
@@ -2,19 +2,19 @@ import passport from "passport";
 import passportLocal from "passport-local";
 import Account from "../models/Account";
 import session, { MemoryStore } from "express-session";
+import dotenv from "dotenv";
 
 const LocalStrategy = passportLocal.Strategy;
+dotenv.config();
 
 export default function (app) {
-	app.use(session({ secret: "DEBUGSECRET" /* TODO: 비밀키 지정 */, resave: true, saveUninitialized: true, store: new MemoryStore() }));
+	app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true, store: new MemoryStore() }));
 	app.use(passport.initialize());
 	app.use(passport.session());
 
 	passport.use(new LocalStrategy(
 		{ usernameField: "id" },
 		async function (username, password, done) {
-			// TODO: 유저 디비 인증 로직
-
 			const result = await Account.login({ userId: username, password });
 			if (result === false) return done(null, false); // 로그인 실패
 
