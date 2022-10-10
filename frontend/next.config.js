e7591b99d6cbdc3789d86068cf2ab5c154e03f2c
Kim Ji-hwan <kjhwan0802@naver.com>
2022-10-10 19:33:18 +0900

    Fix : action fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,12 @@ const nextConfig = {
 
   assetPrefix:
 	process.env.NODE_ENV === 'production'
-		? '' : '',
+		? 'https://Turtle-Hwan.github.io/WEB_MiliDream_Dreamers/' : '',
+  
+  images: {
+	loader: 'imgix',
+	path: 'https://Turtle-Hwan.github.io/WEB_MiliDream_Dreamers/',
+  },
 }
 
 module.exports = nextConfig
