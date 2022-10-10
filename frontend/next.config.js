95c3151c3b8aa47224b0162be65a541dc2a244e6
Kim Ji-hwan <kjhwan0802@naver.com>
2022-10-10 19:58:09 +0900

    [GITHUB PAGES] Add github action for gh-pages (#43)
    * Feat : github pages deploy setting for github action
    
    - github action을 통한 github pages 배포 자동화
    
    * Feat : github pages deploy setting for github action 
    
    - github action 위한 설정값들 추가
    
    * Fix : github action
    
    - frontend 페이지 안에서 배포에 대한 코드 수정
    
    * Fix : github action setting bug fix
    
    * Fix : github action setting bug fix
    
    * Fix : github action setting bug fix
    
    * Fix : github action setting bug fix
    
    * Fix : change yml action file
    
    * Fix : github action
    
    * Fix : deploy err fix
    
    * Fix : action fix
    
    * Fix : real last err fix
    
    * Fix : real last of last fix
    
    * Fix : only for path that osam origin repo
    
    * Fix : 오타 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  assetPrefix:
+	process.env.NODE_ENV === 'production'
+		? 'https://osamhack2022.github.io/WEB_MiliDream_Dreamers/' : '',
+  
+  images: {
+	loader: 'imgix',
+	path: 'https://osamhack2022.github.io/WEB_MiliDream_Dreamers/',
+  },
 }
 
 module.exports = nextConfig
