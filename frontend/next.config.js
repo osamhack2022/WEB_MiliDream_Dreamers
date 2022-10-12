a6ee226ca10469a574d6b33481cc6dcf41b9147d
inyeoptti <inyeoptti@github.com>
2022-10-12 14:37:24 +0000

    Feat: api 프록시 코드를 추가했습니다

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+          destination: process.env.DESTINATION_URL,
+          source: process.env.SOURCE_PATH,
+      },
+  ];
+  }
 }
 
 module.exports = nextConfig
