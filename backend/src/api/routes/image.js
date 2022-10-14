a4ba006765d7273e96af49d1d88af2ef8cfddfcb
inyeoptti <48206925+inyeoptti@users.noreply.github.com>
2022-10-14 23:20:20 +0900

    Update image.js

--- backend/src/api/routes/image.js
+++ backend/src/api/routes/image.js
@@ -3,19 +3,10 @@ import { upload } from "../../loaders/multer";
 
 const router = Router();
 
-// POST /image/upload-avatar
-// Content-Type: multipart/form-data
-// Requires field(name=avatar) with image data
-router.post("/upload-avatar", upload.single('avatar'), (req, res) => {
-    const { fieldname, originalname, encoding, mimetype, destination, filename, path, size } = req.file;
-
-    res.json({ path: `/public/${filename}` });
-});
-
 // POST /image/upload
 // Content-Type: multipart/form-data
-// Requires field(name=postImages) with image data
-router.post("/upload", upload.single('postImages'), (req, res) => {
+// Requires field(name=img) with image data
+router.post("/upload", upload.single('img'), (req, res) => {
     const { fieldname, originalname, encoding, mimetype, destination, filename, path, size } = req.file;
 
     res.json({ path: `/public/${filename}` });
