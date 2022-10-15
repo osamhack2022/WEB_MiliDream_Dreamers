a6272d49728da54bbff70bd8fd36e52086b8b365
inyeoptti <inyeoptti@github.com>
2022-10-15 13:08:44 +0900

    [#44] Feat: file==undefined 일시 에러 추가

--- backend/src/api/routes/image.js
+++ backend/src/api/routes/image.js
@@ -7,6 +7,8 @@ const router = Router();
 // Content-Type: multipart/form-data
 // Requires field(name=img) with image data
 router.post("/upload", upload.single('img'), (req, res) => {
+    if(!req.file) return res.status(400).json("File Attachment is undefined");
+
     const { fieldname, originalname, encoding, mimetype, destination, filename, path, size } = req.file;
 
     res.json({ path: `/images/${filename}` });
