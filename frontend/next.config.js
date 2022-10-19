2aaf83192f9e6a0fe18f25bc620a0f15491bf6cd
inyeoptti <inyeoptti@github.com>
2022-10-19 22:39:00 +0900

    Feat: next.config.js에 basePath 옵션 추가

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,4 @@
+const isProduction = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -5,19 +6,18 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-          destination: process.env.DESTINATION_URL,
-          source: process.env.SOURCE_PATH,
+        destination: process.env.DESTINATION_URL,
+        source: process.env.SOURCE_PATH,
       },
-  ];
+    ];
   },
-  assetPrefix:
-	process.env.NODE_ENV === 'production'
-		? 'https://osamhack2022.github.io/WEB_MiliDream_Dreamers/' : '',
-  
+  assetPrefix: isProduction ? 'https://osamhack2022.github.io/WEB_MiliDream_Dreamers/' : '',
+
   images: {
-	loader: 'imgix',
-	path: 'https://osamhack2022.github.io/WEB_MiliDream_Dreamers/',
+    loader: 'imgix',
+    path: 'https://osamhack2022.github.io/WEB_MiliDream_Dreamers/',
   },
+  basePath: '/WEB_MiliDream_Dreamers'
 }
 
 module.exports = nextConfig
