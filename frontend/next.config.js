4ba83bb21720ffbb5cee58af818f8ddf61ff4e7f
inyeoptti <inyeoptti@github.com>
2022-10-20 21:47:42 +0900

    Revert "Fix: add imagesPublicPath option"
    This reverts commit 42332f252e21dc6020e6ce5519e37e5754feeea9.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,29 +1,23 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-
 const isProduction = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
-const nextConfig = withPlugins([
-	[optimizedImages,
-		{ imagesPublicPath: '/WEB_MiliDream_Dreamers/' }],
-	{
-		reactStrictMode: true,
-		swcMinify: true,
-		async rewrites() {
-			return [
-				{
-					destination: process.env.DESTINATION_URL,
-					source: process.env.SOURCE_PATH,
-				},
-			];
-		},
-		assetPrefix: isProduction ? '/WEB_MiliDream_Dreamers/' : '',
-		images: {
-			loader: 'imgix',
-			path: '/WEB_MiliDream_Dreamers/',
-		},
-		basePath: '/WEB_MiliDream_Dreamers',
-	}
-]);
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        destination: process.env.DESTINATION_URL,
+        source: process.env.SOURCE_PATH,
+      },
+    ];
+  },
+  assetPrefix: isProduction ? 'https://osamhack2022.github.io/WEB_MiliDream_Dreamers/' : '',
 
-module.exports = nextConfig;
+  images: {
+    loader: 'imgix',
+    path: 'https://osamhack2022.github.io/WEB_MiliDream_Dreamers/',
+  },
+  basePath: '/WEB_MiliDream_Dreamers'
+}
+
+module.exports = nextConfig
