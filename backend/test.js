cf2d89f521f8ef537a126b8ec11f1389979fab79
inyeoptti <inyeoptti@github.com>
2022-10-22 12:42:57 +0900

    Feat: 테스트 코드 추가(/user)

--- backend/test.js
+++ backend/test.js
@@ -12,11 +12,50 @@ const client = wrapper(axios.create(axiosConfig));
 let testData = {};
 
 async function test() {
-	test_accounts();
+	//await test_accounts();
+	const testAccount = await getTestAccount();
+	await test_user();
+	await deleteTestAccount();
+}
+
+async function getTestAccount() {
+	let testData = {};
+	testData.userId = await randomText(5);
+	testData.userName = await randomText(5);
+	testData.password = await randomText(20);
+	const dataLogEnabled = false;
+	try { // 토큰
+		let tokenRequest = await client.get(baseUrl + "accounts/signup-token");
+		expect(tokenRequest.status).toBe(200);
+		let responseData = tokenRequest.data;
+		expectRegex(responseData?.token).toBe(/^[0-9a-f]{16}$/);
+		testData.token = responseData?.token ?? "DEBUG";
+	}
+	catch (ex) { console.error(ex.message); }
+	try { // 회원가입
+		let registerRequest = await client.post(baseUrl + "accounts/account",
+			{ userId: testData.userId, password: testData.password, userName: testData.userName, userClass: 1, token: testData.token }
+		);
+		let responseData = registerRequest.data;
+		dataLogEnabled && console.log(responseData);
+		expect(registerRequest.status).toBe(200);
+	}
+	catch (ex) { console.error(ex.message); }
+	try { // 로그인
+		let loginRequest = await client.post(baseUrl + "accounts/sign",
+			{ id: testData.userId, password: testData.password }
+
+		);
+		let responseData = loginRequest.data;
+		dataLogEnabled && console.log(responseData);
+		expect(loginRequest.status).toBe(200);
+		return testData;
+	}
+	catch (ex) { console.error(ex.message); }
+	return undefined;
 }
 
 async function test_accounts() {
-	let lastToken = "";
 	const dataLogEnabled = false;
 	try { // 토큰
 		let tokenRequest = await client.get(baseUrl + "accounts/signup-token");
@@ -133,6 +172,55 @@ async function test_accounts() {
 		console.log("[DELETE /accounts/account] Success");
 	}
 	catch (ex) { console.error(ex.message); }
+}
+
+async function deleteTestAccount() {
+	try { // 회원탈퇴
+		let removeAccountRequest = await client.delete(baseUrl + "accounts/account");
+		expect(removeAccountRequest.status).toBe(200);
+	}
+	catch (ex) { console.error(ex.message); }
+}
+
+async function test_user() {
+	const dataLogEnabled = true;
+	let userId;
+
+	try { // GET /user -> expected return 401 when not logon
+		let userRequest = await axios.get(baseUrl + "user", { validateStatus: false });
+		let responseData = userRequest.data;
+		dataLogEnabled && console.log(responseData);
+		expect(userRequest.status).toBe(401);
+
+		console.log("[GET /user noauth] Success");
+	}
+	catch (ex) { console.error(ex.message); }
+
+	try { // GET /user -> expected return redirect GET /user/{:userId}
+		let userRequest = await client.get(baseUrl + "user");
+		let responseData = userRequest.data;
+		dataLogEnabled && console.log(responseData);
+		expect(userRequest.status).toBe(200);
+		userId = userRequest.request?.path.split("/")[2] * 1;
+
+		console.log(`[GET /user auth] Success->${userId}`);
+	}
+	catch (ex) { console.error(ex.message); }
+
+	try { // PUT /user/:userId -> change password
+		const new_password = await randomText(20);
+		let userRequest = await client.put(baseUrl + "user/" + userId,
+			{ new_password }
+		);
+		let responseData = userRequest.data;
+		dataLogEnabled && console.log(responseData);
+		expect(userRequest.status).toBe(200);
+
+		console.log(`[POST /user/${userId} auth] Success->pw:${new_password}`);
+	}
+	catch (ex) { console.error(ex.message); }
+
+	/** @TODO test avatar upload */
 }
 
 function expect(result) {
