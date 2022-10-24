dc19f84c5cb256b8c30067b8ff7cfedd7d0ebd7f
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-10-24 06:07:06 +0000

    [#31] Fix : 오류 수정

--- backend/base.sql
+++ backend/base.sql
@@ -27,7 +27,7 @@ create table Post (
 	body TEXT NOT NULL,
 	categoryKey INT NOT NULL,
 	viewCount INT NOT NULL DEFAULT 0,
-	FOREIGN KEY(userKey) REFERENCES User(userKey) ON UPDATE RESTRICT ON DELETE SET NULL
+	FOREIGN KEY(userKey) REFERENCES User(userKey) ON UPDATE RESTRICT ON DELETE SET NULL,
 	FOREIGN KEY(categoryKey) REFERENCES Category(categoryKey) ON UPDATE RESTRICT ON DELETE CASCADE
 );
 
@@ -87,5 +87,5 @@ select * from Post;
 insert into Comment(userKey, body, postKey, parentKey, commentTime) values (1, "HIHI", 1, NULL, "2022-10-09  10:47:36 "), (2, "HELLO", 1, 1, "2022-10-09  10:48:21 "), (2, "BYE", 1, NULL, "2022-10-09 10:52:13 ");
 insert into Comment(userKey, body, postKey, parentKey, commentTime) values (1, "HIHI2", 2, NULL, "2022-10-09 15:06:09"), (2, "HELLO2", 2, 1, "2022-10-09 15:36:43"), (2, "BYE2", 1, NULL, "2022-10-09 16:13:41");
 
-insert into Purpose(userKey, tltle, progress, explanation) values (1, "목표 테스트", "진행중", "목표 테스트 설명");
-select * from Purpose;
+insert into Objectives(userKey, title, progress, explanation) values (1, "목표 테스트", "진행중", "목표 테스트 설명");
+select * from Objectives;
