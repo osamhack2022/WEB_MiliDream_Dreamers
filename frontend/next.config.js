f8908b5754375a416caa014a72c9ddb56875b46c
Jinhong Min <pappies0827@gmail.com>
2022-10-25 14:26:55 +0900

    Next-images 모듈 가져오기

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,7 @@
+/** next-images module 가져오기 */
+const withImages = require('next-images');
+module.exports = withImages();
+
 const isProduction = process.env.NODE_ENV === 'production';
 const isGhPage = process.env.DEPLOY_METHOD === 'gh-page';
 /** @type {import('next').NextConfig} */
