54ef052924884024727f08b141a49a9c97fab289
Kim Ji-hwan <kjhwan0802@naver.com>
2022-10-28 14:13:54 +0000

    Fix : Link 라이브러리 추가

--- frontend/components/Home/Service_2.js
+++ frontend/components/Home/Service_2.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Service_2() {
 	return (
