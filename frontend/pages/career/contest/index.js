843a696c271b5b9dbfe1805c182a447793a13df0
Kim Ji-hwan <kjhwan0802@naver.com>
2022-10-30 10:58:14 +0000

    Chore : 공모전 검색 기능 누르면 모달창 띄우도록.

--- frontend/pages/career/contest/index.js
+++ frontend/pages/career/contest/index.js
@@ -4,8 +4,28 @@ import Image from "next/image";
 export default function Contact() {
 	return (
 		<div className="container">
+			<div className="modal fade" id="TodoCoding" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h1 className="modal-title fs-5" id="exampleModalLabel">개발 중 입니다!</h1>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div className="modal-body">
+							<p>아직 개발 중인 기능입니다. issue를 남겨 주세요! </p>
+							<p>https://github.com/osamhack2022/WEB_MiliDream_Dreamers/issues</p>
+						</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">확인</button>
+							{/* <button type="button" className="btn btn-primary">Save changes</button> */}
+						</div>
+					</div>
+				</div>
+			</div>
 			<div className="searchBar">
-				<Image width="1297px" height="296px" src={`/contest/list-searchBar.png`} />
+				<a type="button" data-bs-toggle="modal" data-bs-target="#TodoCoding">
+					<Image width="1297px" height="296px" src={`/contest/list-searchBar.png`} />
+				</a>
 			</div>
 			<div className="result">
 				<Image width="1237px" height="38px" src={`/contest/searchResult.png`} />
@@ -22,6 +42,12 @@ export default function Contact() {
 				</Link>
 			</div>
 			<style jsx>{`
+				.modal-content {
+					width: 130%;
+				}
+				.button {
+					cursor: default;;
+				}
 				.container {
 					display: flex;
 					flex-direction: column;
@@ -32,6 +58,7 @@ export default function Contact() {
 				}	
 				.list {
 					margin-left: 30px;
+					cursor: pointer;
 				}
 			`}</style>
 		</div>
