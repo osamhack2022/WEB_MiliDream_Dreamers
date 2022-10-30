7d510b25db00d312ed9e6f724ec1f3cae6ef1b44
Kim Ji-hwan <kjhwan0802@naver.com>
2022-10-30 06:52:02 +0000

    Fix : 조금 더 완벽해진 (undefined를 없앤) title 처리 ㅎ

--- frontend/components/Seo.js
+++ frontend/components/Seo.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 
 export default function Seo({ title }) {
-	let titleArray = { '/': '메인', '/map': '진로 설계', '/board': '게시판', '/career/contest': '공모전', '/career/survey': '유형 검사', '/signup': '회원가입', '/user': '프로필', };
+	let titleArray = { '/': '메인', '/map': '진로 설계', '/board': '게시판', '/career/contest': '공모전', '/career/survey': '유형 검사', '/signup': '회원가입', '/user': '프로필', '/board/search': '게시판 검색 결과' };
 	//console.log(title.indexOf('/', 1));
 	//console.log(titleArray[title])
 	let tabTitle = titleArray[title];
+	if (titleArray[title] === undefined) {
+		tabTitle = titleArray[title.substr(0, title.indexOf('/', 1))]
+	}
 
 	return (
 		<Head>
