ffb7b444468fe4d5ab74bab2e2652506526e4d48
Kim Ji-hwan <kjhwan0802@naver.com>
2022-10-30 06:47:15 +0000

    Fix : tab Title 수정

--- frontend/components/Seo.js
+++ frontend/components/Seo.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
 
 export default function Seo({ title }) {
-    return (
-        <Head>
-            <title>{`MILI-Dream | ${title}`}</title>
-        </Head>
-    )
+	let titleArray = { '/': '메인', '/map': '진로 설계', '/board': '게시판', '/career/contest': '공모전', '/career/survey': '유형 검사', '/signup': '회원가입', '/user': '프로필', };
+	//console.log(title.indexOf('/', 1));
+	//console.log(titleArray[title])
+	let tabTitle = titleArray[title];
+
+	return (
+		<Head>
+			<title>{`MILI-Dream | ${tabTitle}`}</title>
+		</Head>
+	)
 }
